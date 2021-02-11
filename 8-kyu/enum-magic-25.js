0e35503907245f3066891e6e38be1267361f5b4c
Kris <kris@syevens.com>
2021-02-10 22:37:06 -0600

    Update Commit - 8kyu JS Chall

---
+++ 8-kyu/enum-magic-25.js
@@ -0,0 +1,26 @@
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+function take(arr, n) {
+  // Your code here
+  let finalArr = [];
+
+  if (n === 0 || !arr.length) return finalArr;
+
+  if (n <= arr.length) {
+    for (let i = 0; i < n; i++) {
+      finalArr.push(arr[i]);
+    }
+  } else if (n > arr.length) {
+    for (let i = 0; i < arr.length; i++) {
+      finalArr.push(arr[i]);
+    }
+  }
+
+  return finalArr;
+}
+
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
+
+// function take(arr, n) {
+//   return arr.slice(0, n);
+// }
