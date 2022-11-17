e478299685a7ef0b91b43aec0fccd248f641060a
Kris <kris@syevens.com>
2022-11-17 09:13:45 -0600

    Update Commit
    Added 6kyu challenge.

---
+++ 6-kyu/unique-order.js
@@ -0,0 +1,19 @@
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+let uniqueInOrder = (iterable) => {
+  return [...iterable].filter((el, i, arr) => {
+    if (el !== arr[i + 1]) {
+      return el;
+    }
+  });
+};
+
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder("ABBCcAD"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
