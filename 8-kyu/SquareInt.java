9457967670929b32e7f21f43c61c00df2c88f8ec
Kris <kris@syevens.com>
2022-11-25 12:40:41 -0600

    Update Commit - Added 8kyu java chall

---
+++ 8-kyu/SquareInt.java
@@ -0,0 +1,22 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+public class SquareInt
+ {
+  public static int square(int n){
+    return n * n;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(square(2));
+  }
+}
+
+
+
+// public class Kata
+//  {
+//   public static int square(int n){
+//     return (int) Math.pow(n, 2);
+//   }
+//  }
