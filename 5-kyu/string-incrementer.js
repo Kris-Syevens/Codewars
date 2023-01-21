4243742ae91135d229752241c0f91c6b7d663a41
Kris <kris@syevens.com>
2023-01-21 00:32:27 -0600

    Update Commit - 5kyu JS Chall

--- 5-kyu/string-incrementer.js
+++ 5-kyu/string-incrementer.js
@@ -20,7 +20,52 @@
 // REGULAR EXPRESSIONS STRINGS
 
 function incrementString(string) {
-  // return incrementedString
+  if (string.length === 1) {
+    if (string[0].search(/[0-9]/g) !== -1) {
+      return `${parseInt(string[0]) + 1}`;
+    } else return `${string[0]}1`;
+  }
+
+  if (string.search(/[0-9]/g) !== -1 && string.search(/[a-z]/g) === -1) {
+    let finalNumStr = `${parseInt(string) + 1}`.split("");
+
+    while (finalNumStr.length < string.length) {
+      finalNumStr.unshift("0");
+    }
+
+    return `${finalNumStr.join("")}`;
+  }
+
+  if (string.search(/[0-9]/g) !== -1) {
+    // There is a number in the string
+
+    const numStr = string.slice(
+      string.lastIndexOf(
+        string.match(/[a-z]/gi)[string.match(/[a-z]/gi).length - 1]
+      ) + 1,
+      string.length
+    );
+
+    let finalNumStr = `${parseInt(numStr) + 1}`.split("");
+
+    while (finalNumStr.length < numStr.length) {
+      finalNumStr.unshift("0");
+    }
+
+    return `${string.slice(0, string.length - numStr.length)}${finalNumStr.join(
+      ""
+    )}`;
+
+    //
+  } else return `${string}1`;
 }
 
 console.log(incrementString("foobar000")); // foobar001
+console.log(incrementString("foobar999")); // foobar1000
+console.log(incrementString("foobar00999")); // foobar01000
+console.log(incrementString("foo")); // foo1
+console.log(incrementString("foobar001")); // foobar002
+console.log(incrementString("foobar1")); // foobar2
+console.log(incrementString("1")); // 2
+console.log(incrementString("009")); // 010
+console.log(incrementString("fo99obar99")); // fo99obar100
