d5a717d75e2c325fb491ecc6a97d0f121d10d558
Kris <kris@syevens.com>
2023-01-28 17:33:08 -0600

    Update Commit - 8kyu JS Chall

---
+++ 8-kyu/loopFix.js
@@ -0,0 +1,26 @@
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+// DEBUGGING
+
+// function createArray(number) {
+//   var newArray = [];
+
+//   for (var counter = 1; counter <= number; ) {
+//     newArray.push(counter);
+//   }
+
+//   return newArray;
+// }
+
+function createArray(number) {
+  var newArray = [];
+
+  for (var counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
+
+console.log(createArray(7));
