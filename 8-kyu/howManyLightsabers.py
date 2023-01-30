9cf15b162845bfbe0d0c79290550321f4373faf5
Kris <kris@syevens.com>
2023-01-30 15:22:18 -0600

    Update Commit - 8kyu Py Chall

---
+++ 8-kyu/howManyLightsabers.py
@@ -0,0 +1,24 @@
+# DESCRIPTION:
+# Inspired by the development team at Vooza, write the function that
+
+# accepts the name of a programmer, and
+# returns the number of lightsabers owned by that person.
+# The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+# Note: your function should have a default parameter.
+
+# For example(Input - -> Output):
+
+# "anyone else" - -> 0
+# "Zach" - -> 18
+# FUNDAMENTALS
+
+
+def howManyLightsabersDoYouOwn(*name):
+    return 18 if name == ('Zach',) else 0
+
+
+print(howManyLightsabersDoYouOwn("Zach"))
+print(howManyLightsabersDoYouOwn())
+print(howManyLightsabersDoYouOwn("Kris"))
+print(howManyLightsabersDoYouOwn("zach"))
