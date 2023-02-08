32710acd8cc2d4537ee33346b0b68b0e36c22f74
Kris <kris@syevens.com>
2023-02-07 22:41:43 -0600

    Update Commit - 8kyu Java Chall

---
+++ 8-kyu/KataChromoCheck.java
@@ -0,0 +1,26 @@
+public class KataChromoCheck {
+
+//    The male gametes or sperm cells in humans and other mammals are hetero-gametic and contain one of two types of sex chromosomes. They are either X or Y. The female gametes or eggs however, contain only the X sex chromosome and are homo-gametic.
+//
+//    The sperm cell determines the sex of an individual in this case. If a sperm cell containing an X chromosome fertilizes an egg, the resulting zygote will be XX or female. If the sperm cell contains a Y chromosome, then the resulting zygote will be XY or male.
+//
+//    Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
+//
+//    If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+    public static String chromosomeCheck(String sperm) {
+        // Your code goes here. Have fun!
+        if (sperm.equals("XX")) {
+            return "Congratulations! You're going to have a daughter.";
+        } else if (sperm.equals("XY")) {
+            return "Congratulations! You're going to have a son.";
+        } else return "Invalid option. Sperm must be either XY or XX.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(chromosomeCheck("XX")); // Daughter
+        System.out.println(chromosomeCheck("XY")); // Son
+        System.out.println(chromosomeCheck("ZY")); // Invalid
+    }
+
+}
